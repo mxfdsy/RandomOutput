e17e0ae6d0e3cd0df5ced625cacfb81c449678fa
平凡的世界 <963559210@qq.com>
2018-03-17 15:48:39 +0800

    完成需求，实现26个英文字母，5个一位随机输出（但是和我第一次在公司写的不一样好像要垃圾点）

--- src/RandomOutput.java
+++ src/RandomOutput.java
@@ -2,26 +2,36 @@
  * 需求随机生成26的英文字母，5个一输出
  * Created by 平凡的世界 on 2018/3/16.
  */
+import jdk.nashorn.internal.ir.WhileNode;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class RandomOutput {
     public static void main(String[] args) {
-        randomOutput(97, 123);
+        randomOutput(97, 123,5);
     }
-            private static void randomOutput(int min, int max){
+
+            private static void randomOutput(int min, int max,int len){
                 List<Character> list = new LinkedList<>();
                 for (int i = min; i < max; i++) {
                     list.add((char)i);
                 }
                 int size = list.size();
 
+                StringBuilder sb = new StringBuilder(26);
                 for (int i = 0; i < max - min; i++) {
                     int index = new Random().nextInt(size);
-                    System.out.println("第" + (i + 1) + "个字母 " + list.remove(index));
+//                  System.out.println("第" + (i + 1) + "个字母 " + list.remove(index));
+                    while (!(sb.length() != len)) {
+                        System.out.println(sb.toString());
+                        sb.replace(0,len,"");
+                    }
+                    sb.append(list.remove(index));
                     size--;
                 }
+                System.out.println(sb.toString());
             }
 
     }
